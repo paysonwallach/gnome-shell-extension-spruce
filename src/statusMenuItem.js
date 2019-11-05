617097572650695ae7564dbc9847d4b3efe631a3
Payson Wallach <paysonwallach@icloud.com>
2019-11-04 21:46:24 -0600

    Arrange menu items

--- src/statusMenuItem.js
+++ src/statusMenuItem.js
@@ -23,6 +23,8 @@ const Gettext = imports.gettext.domain(spruce.metadata["gettext-domain"]);
 const _ = Gettext.gettext;
 
 const Commands = spruce.imports.commands;
+const WindowActions = spruce.imports.windowActions;
+
 const {moveFocusedWindow} = spruce.imports.windowTransformHandler;
 const settings = imports.misc.extensionUtils.getSettings(
   spruce.metadata["settings-schema"]
@@ -32,8 +34,7 @@ var StatusMenuItem = class StatusMenuItem {
   constructor() {}
 
   enable() {
-    this.button = new PanelMenu.Button(0, spruce.metadata["name"], false);
-    this.button.accessible_role = Atk.Role.TOGGLE_BUTTON;
+    const MENU_SEPARATOR_ITEM = "menu-separator-item";
 
     const gIcon = Gio.icon_new_for_string(
       "resource:///org/gnome/shell/extensions/spruce/icons/spruce-symbolic.svg"
@@ -43,24 +44,62 @@ var StatusMenuItem = class StatusMenuItem {
       style_class: "system-status-icon"
     });
 
+    this.button = new PanelMenu.Button(0, spruce.metadata["name"], false);
+    this.button.accessible_role = Atk.Role.TOGGLE_BUTTON;
+
     this.button.add_child(icon);
     this.button.add_style_class_name("panel-status-button");
 
+    const commands = [];
     for (const command in Commands) {
-      const action = spruce.imports.commands[command];
-      const menuItem = new PopupMenu.PopupMenuItem(_(action.name));
-      const handler = moveFocusedWindow.bind(this, action);
+      commands.push(spruce.imports.commands[command]);
+    }
+    const menuItems = [
+      WindowActions.CENTER,
+      WindowActions.FULLSCREEN,
+      MENU_SEPARATOR_ITEM,
+      WindowActions.LEFT_EDGE,
+      WindowActions.RIGHT_EDGE,
+      WindowActions.TOP_EDGE,
+      WindowActions.BOTTOM_EDGE,
+      MENU_SEPARATOR_ITEM,
+      WindowActions.UPPER_LEFT_CORNER,
+      WindowActions.LOWER_LEFT_CORNER,
+      WindowActions.UPPER_RIGHT_CORNER,
+      WindowActions.LOWER_RIGHT_CORNER,
+      MENU_SEPARATOR_ITEM,
+      WindowActions.NEXT_THIRD,
+      WindowActions.PREVIOUS_THIRD,
+      MENU_SEPARATOR_ITEM,
+      WindowActions.NEXT_DISPLAY,
+      WindowActions.PREVIOUS_DISPLAY,
+      MENU_SEPARATOR_ITEM,
+      WindowActions.LARGER,
+      WindowActions.SMALLER
+    ];
 
-      menuItem.connect("activate", handler);
+    for (const item of menuItems) {
+      let menuItem;
+
+      if (item === MENU_SEPARATOR_ITEM) {
+        menuItem = new PopupMenu.PopupSeparatorMenuItem();
+      } else {
+        const action = commands.filter(command => command.name === item)[0];
+        const handler = moveFocusedWindow.bind(this, action);
+
+        Main.wm.addKeybinding(
+          action.name,
+          settings,
+          Meta.KeyBindingFlags.NONE,
+          Shell.ActionMode.NORMAL,
+          handler
+        );
+
+        menuItem = new PopupMenu.PopupMenuItem(_(action.name));
+        menuItem.connect("activate", handler);
+      }
+
       this.button.menu.addMenuItem(menuItem);
-
-      Main.wm.addKeybinding(
-        action.name,
-        settings,
-        Meta.KeyBindingFlags.NONE,
-        Shell.ActionMode.NORMAL,
-        handler
-      );
     }
 
     Main.panel.addToStatusArea("spruce-menu", this.button, 0, "right");
